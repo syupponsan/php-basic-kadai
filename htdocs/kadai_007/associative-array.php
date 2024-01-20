1e3f09f4dac807e90a7d3505315358730c982f9c
syupponsan <145895343+syupponsan@users.noreply.github.com>
2024-01-20 22:53:53 +0900

    Add files via upload

---
+++ htdocs/kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP基礎編</title>
+    </head>
+    <body>
+        <p>
+            <?php
+            $onion_data=['name'=>'onion','price'=>200,'weight'=>160];
+            print_r($onion_data);
+            ?>
+        </P>
+    </body>
+</html>
